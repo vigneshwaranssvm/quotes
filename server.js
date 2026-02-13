b31e78399619bf5b952820fea2621959d2a23cc8
vigneshwaranssvm <vigneshwaranssvm@gmail.com>
2026-02-13 21:59:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let quotes = [];
+
+// Add Quote
+app.post("/add", (req, res) => {
+  const newQuote = {
+    id: Date.now(),
+    name: req.body.name,
+    quote: req.body.quote
+  };
+
+  quotes.push(newQuote);
+  res.json(newQuote);
+});
+
+// Get All Quotes
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
+
+// Delete Quote
+app.delete("/delete/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  quotes = quotes.filter(q => q.id !== id);
+  res.json({ message: "Deleted successfully" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+
