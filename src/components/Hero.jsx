c537af06b05f5864d7531bfefaa8874c74b05d38
vigneshwaranssvm <vigneshwaranssvm@gmail.com>
2026-02-13 19:14:00 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import "../styles/hero.css";    
+import "../styles/Hero.css";    
 
 const Hero = ({ name, quote }) => {
 
