5fda5cc16520735f6be7c0f7a3c560302729b310
vigneshwaranssvm <vigneshwaranssvm@gmail.com>
2026-02-14 13:55:49 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://quotes-backend-gzap.onrender.com
+  baseURL:https://quotes-backend-gzap.onrender.com
 });
 
 export default API;
